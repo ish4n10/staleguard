087a1a09d0553c32be20f91edec7620d1a253d8a
ish4n10 <ishan.tripathi010@gmail.com>
2026-06-20 21:49:14 +0530

    Update __init__.py

--- src/adapters/__init__.py
+++ src/adapters/__init__.py
@@ -1,3 +1,4 @@
+from .chroma import normalize_chroma_result
 from .json_adapter import normalize_chunk, normalize_chunks
 
-__all__ = ["normalize_chunk", "normalize_chunks"]
+__all__ = ["normalize_chunk", "normalize_chunks", "normalize_chroma_result"]
